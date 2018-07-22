642a160b6f13439c6f9adbe6e59b154b3d97d666
Philip Ford <pford68@earthlink.net>
2018-07-22 12:48:58 -0400

    Decorators

---
+++ src/js/next/decorators.js
@@ -0,0 +1,39 @@
+/**
+ *
+ */
+'use strict';
+
+//alert('OK');
+
+function log(target, name, descriptor) {
+    const original = descriptor.value;
+    if (typeof original === 'function') {
+        descriptor.value = function(...args) {
+            console.log(`Arguments: ${args}`);
+            try {
+                const result = original.apply(this, args);
+                console.log(`Result: ${result}`);
+                return result;
+            } catch (e) {
+                console.log(`Error: ${e}`);
+                throw e;
+            }
+        };
+    }
+    return descriptor;
+}
+
+
+class Example {
+    /* jshint ignore:start */
+    @log
+    /* jshint ignore:end */
+    sum(a, b) {
+        return a + b;
+    }
+}
+
+const e = new Example();
+e.sum(1, 2);
+// Arguments: 1,2
+// Result: 3
