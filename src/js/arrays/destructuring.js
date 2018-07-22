3a9ed2f72ea7cac23452fd4da24cbe0aa0302328
Philip Ford <pford68@earthlink.net>
2018-07-21 23:40:01 -0400

    Object destructuring

--- src/js/arrays/destructuring.js
+++ src/js/arrays/destructuring.js
@@ -9,3 +9,17 @@ const [one, two, three] = myArray;
 console.log('Destructuring...');
 console.log(myArray);
 console.log(one, two, three);
+
+
+
+const myObject = {
+    firstName:   'a1',
+    lastName:   'b1',
+    middle: 'c1'
+};
+
+// ES6 destructuring example
+const {firstName: first, lastName: second, middle: third} = myObject;
+console.log(first, second, third);
+
+
